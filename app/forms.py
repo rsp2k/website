9430ed6a2bbd3cc3f5bcb07cfa98fe3e480a5634
Ryan Malloy <ryan@supported.systems>
2017-03-27 04:00:18 -0600

    fix contact form

--- app/forms.py
+++ app/forms.py
@@ -1,22 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import *
-from wtforms.widgets import Input, TextArea
 from wtforms.validators import Required, Email
-from wtforms import ValidationError
-
-import phonenumbers
-
-
-class TelephoneForm(FlaskForm):
-    country_code = IntegerField('Country Code', validators=[Required()])
-    area_code    = IntegerField('Area Code', validators=[Required()])
-    number       = TextField('Number')
 
 class ContactForm(FlaskForm):
     name = TextField(u'Your name', validators=[Required()])
     email = TextField(u'Your email address', validators=[Email()])
-    phone = FormField(TelephoneForm)
-    message = TextAreaField('Your message:', validators=[Required()],widget=TextArea(rows=70, cols=11))
+    phone = TextField(u'Your phone number')
+    message = TextAreaField('Your message:', validators=[Required()])
 
     submit = SubmitField(u'Signup')
 
