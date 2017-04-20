b1beca0ccd8971d3a7555da8ae7bf9435ab9b0b6
rsp2k <rsp2k@malloys.us>
2017-04-20 01:42:12 +0100

    c

--- app/spark.py
+++ app/spark.py
@@ -1,16 +1,18 @@
-import datetime
-import logging
-
 import ciscosparkapi
-from app import tropo
-
-from flask import url_for
 
 from app import config
+from app import tropo
+
 
 def create_room_webhook(room, webhook_url):
-    # Create the webhook that is triggered when new messages are posted to the room when
-    # a message
+    """
+    Create the webhook if it doesn't already exist. The Webhook is triggered when new messages are posted to the room.
+    :param room: Cisco Spark Room object to create webhook in 
+    :param webhook_url: URL for webhook to POST JSON payload to
+    :return: Cisco Spark Webhook object.
+    """
+    # Create when
+    #  a message
     resource = "messages"
     # is created
     event = "created"
@@ -20,16 +22,25 @@ def create_room_webhook(room, webhook_url):
     # insecure example secret used to generate the payload signature
     secret = webhook_url + '12345'
 
+    # Connect to SparkAPI with SPARK_TOKEN
     spark_api = ciscosparkapi.CiscoSparkAPI(access_token=config.SPARK_TOKEN)
 
-    spark_api.webhooks.create(room.title + 'messages created',
+    # search webhooks items for matching
+    for webhook in spark_api.webhooks.list():
+        if webhook.resource == resource and webhook.event == event and webhook.filter == filter_:
+            break
+    else:
+        webhook = spark_api.webhooks.create(room.title + 'messages created',
             webhook_url, resource, event, filter_, secret)
 
+    return webhook
 
 def customer_room_message_send(customer_id, **room_args):
     """
-    Posts message to customer's Spark Room.
-    Pass the customer #, the Spark Room ID will be looked up/created for you.
+    Posts message to customer's Spark Room for customer_id using room_args. Room/Webhook will be created if necessary.
+    :param customer_id: Customer Phone Number (may start with 1)
+    :param room_args: text/markup/file/webhook_url
+    :return: Cisco Spark Message object 
     """
 
     # Basic sanity checking
@@ -41,20 +52,17 @@ def customer_room_message_send(customer_id, **room_args):
 
     # NOTE: Spaces are still called rooms in the API, confusing, I know :)
 
-    # get all rooms for SPARK_AGENT_TEAM_ID
-    rooms = spark_api.rooms.list(teamId=config.SPARK_AGENT_TEAM_ID)
-
-    # loop through rooms, looking for customer room
-    for room in rooms:
+    # loop through rooms for SPARK_AGENT_TEAM_ID, looking for customer room
+    for room in spark_api.rooms.list(teamId=config.SPARK_AGENT_TEAM_ID):
         # Compare first 10 characters of each
-        if room.title[-10:] is customer_id[:-10]:
+        if is_customers_room(room, customer_id):
             break # stop looping if we found the customers room
 
     # If room is not found and break is never encountered, else block runs
     # http://book.pythontips.com/en/latest/for_-_else.html
     else:
         # New customer
-        room = customer_new_signup(customer_id, config.SPARK_AGENT_TEAM_ID, room_args['text'], room_args['webhook_url'])
+        room = customer_new_signup(spark_api, customer_id, config.SPARK_AGENT_TEAM_ID, room_args['text'], room_args['webhook_url'])
 
     # remove webhook url since spark API doesn't accept it
     del(room_args['webhook_url'])
@@ -64,25 +72,43 @@ def customer_room_message_send(customer_id, **room_args):
 class PassedParametersError(Exception):
     pass
 
-def customer_new_signup(customer_id, team_id, message_from_customer, webhook_url):
+def is_customers_room(room, customer_id):
     """
-    Called when a new signup occurs
-    * post message to customer via SMS and in spark room for agent to see
+    Check if given room belongs to customer_id. Looks at last 10 characters of each
+    :param room: Spark Room Object
+    :param customer_id: Customer Phone Number (may start with 1)
+    :return: True or False
+    """
+    if room.title[-10:] is customer_id[:-10]:
+        return True
+    return False
 
+def customer_new_signup(spark_api, customer_id, team_id, message_from_customer, webhook_url):
+    """
+    Create room/webhook and post message room then send customer SMS
+    :param spark_api: Cisco Spark API Object
+    :param customer_id: Customer Phone Number (may start with 1)
+    :param team_id: Team ID to create Customer Room under
+    :param message_from_customer: Text of message sent from customer when they signed up
+    :param webhook_url: URL to use when creating webhook
+    :return: Cisco Spark Room objecct
     """
 
     # Send message via tropo
     message = "Thanks for signing up! To get in touch, reply to this message or call this number during business hours."
     tropo.send_sms(customer_id, message)
 
-    spark_api = ciscosparkapi.CiscoSparkAPI(access_token=config.SPARK_TOKEN)
-    # create a new team room for the customer
-    # http://ciscosparkapi.readthedocs.io/en/latest/user/api.html#ciscosparkapi.RoomsAPI.create
-    room = spark_api.rooms.create(customer_id, teamId=team_id)
+    for room in spark_api.rooms.list():
+        if is_customers_room(room, customer_id):
+            break
+    else:
+        # create a new team room for the customer
+        # http://ciscosparkapi.readthedocs.io/en/latest/user/api.html#ciscosparkapi.RoomsAPI.create
+        room = spark_api.rooms.create(customer_id, teamId=team_id)
 
     # create webhook for new room
     # http://ciscosparkapi.readthedocs.io/en/latest/user/api.html#ciscosparkapi.WebhooksAPI.create
-    create_room_webhook(room, webhook_url)
+    webhook = create_room_webhook(room, webhook_url)
 
     # OPTIONALLY CALL SMARTSHEET IF VARS ARE FILLED OUT IN app/config.py
     # from app.smartsheet_log import smartsheet_log_signup
--- app/tropo.py
+++ app/tropo.py
@@ -6,7 +6,6 @@ import ciscotropowebapi
 
 from app import config
 
-
 def webhook_process(request):
     """
      POST'd data from Tropo WebAPI on inbound voice/sms call
@@ -24,6 +23,8 @@ def webhook_process(request):
 
     # Check to see if this is a request to send a message
     if 'numberToDial' in tropo_session and 'msg' in tropo_session:
+        print("send_sms request to %s. Message %s" % (tropo_session.numberToDial, tropo_session.msg))
+
         # add a + if necessary
         if tropo_session.numberToDial.startswith('+'):
             number_to_call = tropo_session.numberToDial
@@ -38,22 +39,26 @@ def webhook_process(request):
         # https://www.tropo.com/docs/webapi/quickstarts/mixing-text-voice-single-app/using-message-shortcut
         tropo_response.message(tropo_session.msg, number_to_call, network="SMS")
     # Inbound Voice Call
-    elif tropo_session.fromaddress['network'] is 'voice':
+    elif tropo_session.fromaddress['network'] is 'VOICE':
+        print("Voice call from %s. Redirecting to %s" %
+              (tropo_session.fromaddress['id'], config.CUSTOMER_SERVICE_REDIRECT_DN) )
         # Send voice calls to the contact center DN
         tropo_response.redirect(config.CUSTOMER_SERVICE_REDIRECT_DN)
     # Inbound SMS
     elif tropo_session.initalText:
-        # Avoid circular imports, only import right before use
-        from app import spark
-        # post message to Spark room
-        spark_message = spark.customer_room_message_send(tropo_session.from_['id'], text=tropo_session.initialText)
+        if config.SPARK_TOKEN:
+            # Avoid circular imports, only import right before use
+            from app import spark
+            # post message to Spark room
+            spark_message = spark.customer_room_message_send(tropo_session.from_['id'], text=tropo_session.initialText)
 
-        # Acknowledge receipt/error of message
-        if spark_message:
-            tropo_response.say("Thanks. An agent will get back to you as soon as possible. :)")
+            # Acknowledge receipt/error of message
+            if spark_message:
+                tropo_response.say("Thanks. An agent will get back to you as soon as possible. :)")
+            else:
+                tropo_response.say("There was a problem receiving your request, please try again later.")
         else:
-            tropo_response.say("There was a problem receiving your request, please try again later.")
-
+            print("Inbound SMS received from %s: Message: %s"(tropo_session.initialText, tropo_session.from_['id']))
     else:
         raise InvalidRequestError("Invalid request")
 
